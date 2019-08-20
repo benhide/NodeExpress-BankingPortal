379f9292b81f18dc06c2cb2a83e93a157785356c
benhide <ben.m.hide@googlemail.com>
2019-08-20 13:24:27 +0100

    module 2 commit

--- src/app.js
+++ src/app.js
@@ -9,8 +9,35 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const accountData = fs.readFileSync(
+  path.join(__dirname, "json", "accounts.json", "utf8")
+);
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+  path.join(__dirname, "json", "users.json"),
+  "utf8"
+);
+const user = JSON.parse(userData);
+
 app.get("/", (request, response) => {
-  response.render("index", { title: "Index" });
+  response.render("index", { title: "Account Summary", accounts: "accounts" });
+});
+
+app.get("/savings", (request, response) => {
+  response.render("account", { account: "accounts.savings" });
+});
+
+app.get("/checking", (request, response) => {
+  response.render("account", { account: "accounts.checking" });
+});
+
+app.get("/credit", (request, response) => {
+  response.render("account", { account: "accounts.credit" });
+});
+
+app.get("/profile", (request, response) => {
+  response.render("profile", { user: user[0] });
 });
 
 app.listen(3000, () => {
