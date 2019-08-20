13fab926f75b9deeb3687c6a945ecd62e081ce43
benhide <ben.m.hide@googlemail.com>
2019-08-20 14:43:49 +0100

    module4 complete

--- src/app.js
+++ src/app.js
@@ -4,25 +4,14 @@ const express = require("express");
 
 const app = express();
 
+const { accounts, users, writeJSON } = require("./data");
+
 // SETS AND USE *******************************************************
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-
-// DATA ***************************************************************
-const accountData = fs.readFileSync(
-  path.join(__dirname, "json", "accounts.json"),
-  "utf8"
-);
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-  path.join(__dirname, "json", "users.json"),
-  "utf8"
-);
-const users = JSON.parse(userData);
 
 // ROUTES ************************************************************
 app.get("/", (request, response) => {
@@ -58,13 +47,7 @@ app.post("/transfer", (request, response) => {
     parseInt(accounts[request.body.to].balance) +
     parseInt(request.body.amount, 10);
 
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-
-  fs.writeFileSync(
-    path.join(__dirname, "json/accounts.json"),
-    accountsJSON,
-    "utf8"
-  );
+  writeJSON();
 
   response.render("transfer", { message: "Transfer Completed" });
 });
@@ -78,12 +61,8 @@ app.post("/payment", (request, response) => {
   accounts.credit.balance -= request.body.amount;
   accounts.credit.available += parseInt(request.body.amount, 10);
 
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(
-    path.join(__dirname, "json", "accounts.json"),
-    accountsJSON,
-    "utf8"
-  );
+  writeJSON();
+
   response.render("payment", {
     message: "Payment Successful",
     account: accounts.credit
